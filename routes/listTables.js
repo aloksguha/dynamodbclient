17e422cc07eb1fb699f0b5746f777b0bbf4e46fa
Alok Guha <aguha@tibco.com>
2018-06-12 10:28:04 +0530

    changes

--- routes/listTables.js
+++ routes/listTables.js
@@ -1,29 +1,94 @@
 var express = require('express');
 
 var AWS = require("aws-sdk");
-AWS.config.update({
-    region: "us-east-1",
-    endpoint: "http://localhost:8000"
-});
 
-var dynamodb = new AWS.DynamoDB();
 
-var docClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10'
-});
+var dynamodb = null;
+var docClient = null;
+
+const REGION = "us-east-1";
+const HOST = "http://10.245.1.2";
+const PORT = "8000";
+const USERNAME = "";
+const PASSWORD = "";
+const ACCESSKEY = "";
+
+var currentData = {
+        region: REGION,
+        endpoint: HOST+":"+PORT,
+        username: USERNAME,
+        password: PASSWORD,
+        accesskey: ACCESSKEY
+};
+
+(function initConfig() {
+    AWS.config.update({
+        region: REGION,
+        endpoint: HOST+":"+PORT
+    });
+     dynamodb = new AWS.DynamoDB();
+     docClient = new AWS.DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        httpOptions:{
+            connectTimeout:1000
+        }
+    },function(a){console.log(a)});
+    console.log(' >> ');
+    console.log('AWS server started with default config !!');
+})();
 
 var table = "vagrant_tci_users";
 
 var router = express.Router();
 
+
+router.get('/data/config',function(req, res, next){
+    res.json(currentData);
+});
+
+/* GET home page. */
+router.post('/data/config', function (req, res, next) {
+    var configData = req.body;
+    var _region = configData.region || REGION;
+    var _host = configData.host || HOST;
+    var _port = configData.port || PORT;
+    var _username = configData.username || USERNAME;
+    var _password = configData.password || PASSWORD;
+    var _accessKey = configData.accesskey || ACCESSKEY
+    
+    var newData = {
+        region: _region,
+        endpoint: _host + ':' + _port,
+        username: _username,
+        password: _password,
+        accesskey : _accessKey
+    }
+    console.log(newData);
+    currentData = newData;
+    AWS.config.update(newData);
+    
+    dynamodb = new AWS.DynamoDB();
+    docClient = new AWS.DynamoDB.DocumentClient({
+       apiVersion: '2012-08-10'
+   });
+   console.log('AWS server updated with new config !!');
+   res.json({'msg':'done'});
+
+});
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var param = {}
     dynamodb.listTables(param, function (err, data) {
-        if (err)
+        if (err) {
             res.json(err) // an error occurred
-        else
+        }
+        else {
+            console.log(data);
             res.json(data); // successful response
+        }
+
     });
 });
 
@@ -48,16 +113,13 @@ router.get('/:table', function (req, res, next) {
 
     var params = {
         TableName: req.params.table
-        // Key:{
-        //      "userName": username
-        //     // "title": title
-        // }
     };
     docClient.scan(params, function (err, data) {
         if (err) {
             console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
         } else {
             console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            console.log(data);
             res.json(data);
         }
     });
